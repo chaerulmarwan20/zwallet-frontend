c297a9cfbe981ef2225b49a25e12786c28674289
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2021-05-18 22:51:28 +0700

    fix config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    api: "http://localhost:8080/api/v1",
-    image: "http://localhost:8080/",
+    api: "https://zwallet-banking.vercel.app/api/v1",
+    image: "https://zwallet-banking.vercel.app/",
   },
 };
