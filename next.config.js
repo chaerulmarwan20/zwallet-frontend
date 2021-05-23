ffa1875ab282ecf071cc281ff2fe93b348a7ad36
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2021-05-23 22:26:20 +0700

    chang config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
-// module.exports = {
-//   env: {
-//     api: "http://localhost:8080/api/v1",
-//     image: "http://localhost:8080/",
-//   },
-// };
-
 module.exports = {
   env: {
     api: "https://zwallet-apps.fwdev.online/api/v1",
