f59c32f1b2a08acd4d36551ad133f018c457dd63
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2021-05-29 00:00:01 +0700

    fix config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    api: "http://zwallet-apps.fwdev.online/api/v1",
-    image: "http://zwallet-apps.fwdev.online/",
+    api: "https://zwallet-apps.fwdev.online/api/v1",
+    image: "https://zwallet-apps.fwdev.online/",
   },
 };
